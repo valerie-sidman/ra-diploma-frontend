816b9078588946a4f4305ef9722b83268edf377b
Valerie Sidman <valerie.sidman@gmail.com>
2021-03-29 17:15:08 +0300

    without basename

--- src/components/Base.js
+++ src/components/Base.js
@@ -11,7 +11,7 @@ import ErrorPage from './ErrorPage';
 
 export default function Base() {
   return (
-    <Router basename={process.env.REACT_APP_PUBLIC_URL}>
+    <Router>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
@@ -21,6 +21,6 @@ export default function Base() {
         <Route component={ErrorPage} />
       </Switch>
       <Footer />
-    </Router >
+    </Router>
   )
 }
