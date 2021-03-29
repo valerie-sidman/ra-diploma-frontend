6e0894a67ddfde7c8eca86563ca98386abcf0646
Valerie Sidman <valerie.sidman@gmail.com>
2021-03-29 16:29:19 +0300

    added provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root')
+
+  <Provider>
+    <App/>
+  </Provider>,
+
   document.getElementById('root')
 );
 
