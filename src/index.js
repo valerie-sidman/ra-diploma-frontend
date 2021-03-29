b7f1b686f27ad0347a6df9e920be1c8060bc9c93
Valerie Sidman <valerie.sidman@gmail.com>
2021-03-29 16:32:29 +0300

    deleted provider

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  // document.getElementById('root')
-
-  <Provider>
-    <App/>
-  </Provider>,
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
+
 );
 
 reportWebVitals();
